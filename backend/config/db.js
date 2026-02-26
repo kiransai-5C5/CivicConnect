6d84a6c3c5904483aa57243566239c94b31ed3ba
K. Kiran sai <wrangler434@gmail.com>
2026-02-27 00:13:44 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,10 @@
 // backend/config/db.js
-const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/civix", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("✅ Local MongoDB Connected");
+    console.log("✅ Fake DB Connected (No Real Database Required)");
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
+    console.error(error);
     process.exit(1);
   }
 };
