db0d7bdd8aed9694bb3d78857eb2aeb34e91f742
K. Kiran sai <wrangler434@gmail.com>
2026-02-27 00:03:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
+// db.js temporary for testing
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb+srv://kiransai_434:kiransai@cluster0.mhbuuqn.mongodb.net/civix?appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB Connected Successfully');
   } catch (error) {
     console.error('MongoDB Connection Error:', error.message);
-    process.exit(1);
   }
 };
 
